15bcd56da22513f58e6698257475369464e21857
ValeraGritsay <129192864+ValeraGritsay@users.noreply.github.com>
2023-10-29 15:19:06 +0200

    Lab1GritsayKI304.java

--- Lab1GritsayKI304.java
+++ Lab1GritsayKI304.java
@@ -1,22 +1,23 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Програма для генерації та виведення квадратної матриці з заповнювачами на екран та в файл.
- * @author Valera
- * @since 1.0
+ * Програма для генерації та виведення зубчастого масиву з заповнювачами на екран та в файл.
+ * @author Valeriy Gritsay KI-304
+ * @since 2.0
  */
 public class Lab1GritsayKI304 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Введення розміру квадратної матриці
-        System.out.print("Введіть розмір квадратної матриці: ");
-        int matrixSize = scanner.nextInt();
+        // Введення розміру зубчастого масиву
+        System.out.print("Введіть розмір зубчастого масиву: ");
+        int arraySize = scanner.nextInt();
 
-        if (matrixSize <= 0) {
-            System.out.println("Розмір матриці має бути більше за 0.");
+        if (arraySize <= 0) {
+            System.out.println("Розмір масиву має бути більше за 0.");
             return;
         }
 
@@ -24,69 +25,67 @@ public class Lab1GritsayKI304 {
         System.out.print("Введіть символ-заповнювач масиву: ");
         char fillCharacter = scanner.next().charAt(0);
 
-        char[][] matrix = generateMatrix(matrixSize, fillCharacter);
+        ArrayList<char[]> jaggedArray = generateJaggedArray(arraySize, fillCharacter);
 
-        // Виведення матриці на екран
+        // Виведення зубчастого масиву на екран
         System.out.println("Згенерований масив:");
-        printMatrix(matrix);
+        printJaggedArray(jaggedArray);
 
-        // Запис матриці у текстовий файл
+          // Запис зубчастого масиву у текстовий файл
         try {
-            FileWriter writer = new FileWriter("matrix.txt");
-            writeMatrixToFile(matrix, writer);
-            writer.close();
-            System.out.println("Масив збережено у файлі 'matrix.txt'.");
-        } catch (IOException e) {
-            System.out.println("Помилка при записі у файл.");
-        }
+        FileWriter writer = new FileWriter("array.txt");
+        writeJaggedArrayToFile(jaggedArray, writer);
+        writer.close();
+        System.out.println("Масив збережено у файлі 'array.txt'.");
+    } catch (IOException e) {
+        System.out.println("Помилка при записі у файл.");
     }
+}
 
     /**
-     * Генерує квадратну матрицю з заповнювачами.
+     * Генерує зубчастий масив з заповнювачами.
      *
-     * @param size           Розмір матриці.
+     * @param size           Розмір масиву.
      * @param fillCharacter Символ-заповнювач.
-     * @return Квадратна матриця з заповнювачами.
+     * @return Зубчастий масив з заповнювачами.
      */
-    private static char[][] generateMatrix(int size, char fillCharacter) {
-        char[][] matrix = new char[size][size];
+    private static ArrayList<char[]> generateJaggedArray(int size, char fillCharacter) {
+        ArrayList<char[]> jaggedArray = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i >= j && i >= size - 1 - j) {
-                    matrix[i][j] = fillCharacter;
-                } else {
-                    matrix[i][j] = '0';
-                }
+            char[] row = new char[i + 1];
+            for (int j = 0; j <= i; j++) {
+                row[j] = fillCharacter;
             }
+            jaggedArray.add(row);
         }
-        return matrix;
+        return jaggedArray;
     }
 
     /**
-     * Виводить матрицю на екран.
+     * Виводить зубчастий масив на екран.
      *
-     * @param matrix Матриця для виводу.
+     * @param jaggedArray Зубчастий масив для виводу.
      */
-    private static void printMatrix(char[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+    private static void printJaggedArray(ArrayList<char[]> jaggedArray) {
+        for (char[] row : jaggedArray) {
+            for (char c : row) {
+                System.out.print(c);
             }
             System.out.println();
         }
     }
 
     /**
-     * Записує матрицю у текстовий файл.
+     * Записує зубчастий масив у текстовий файл.
      *
-     * @param matrix Матриця для запису.
-     * @param writer Об'єкт FileWriter для запису у файл.
+     * @param jaggedArray Зубчастий масив для запису.
+     * @param writer      Об'єкт FileWriter для запису у файл.
      * @throws IOException Викидається в разі помилки при записі у файл.
      */
-    private static void writeMatrixToFile(char[][] matrix, FileWriter writer) throws IOException {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                writer.write(matrix[i][j] + " ");
+    private static void writeJaggedArrayToFile(ArrayList<char[]> jaggedArray, FileWriter writer) throws IOException {
+        for (char[] row : jaggedArray) {
+            for (char c : row) {
+                writer.write(c);
             }
             writer.write("\n");
         }
